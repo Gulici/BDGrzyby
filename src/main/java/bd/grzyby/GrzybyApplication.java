2fbdcdb4d8533d122541d105ff0b3b53ba3a95d7
Krzysztof Czerniachowicz <233182@student.pwr.edu.pl>
2025-01-04 18:03:06 +0100

    Spring init

---
+++ src/main/java/bd/grzyby/GrzybyApplication.java
@@ -0,0 +1,13 @@
+package bd.grzyby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrzybyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrzybyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bd/grzyby/GrzybyApplicationTests.java
@@ -0,0 +1,13 @@
+package bd.grzyby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrzybyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

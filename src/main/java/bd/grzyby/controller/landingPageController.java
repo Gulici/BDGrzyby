1439f79c81da1add21c19a1e02cea789d0c9c949
Iwo Staykov <259259@student.pwr.edu.pl>
2025-01-12 01:29:43 +0100

    All basic views

--- src/main/java/bd/grzyby/controller/landingPageController.java
+++ src/main/java/bd/grzyby/controller/landingPageController.java
@@ -3,7 +3,8 @@ package bd.grzyby.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
 @Controller
 public class landingPageController {
     @GetMapping("/landingPage")
@@ -11,5 +12,22 @@ public class landingPageController {
         return "landingPage";
     }
 
+    @RequestMapping("/signin")
+    public String handleSigninButton(){
+        return "signin";
+    }
 
+    @RequestMapping("/authors")
+    public String authors(){
+        return "authors";
+    }
+
+    @RequestMapping("/aboutproject")
+    public String aboutProject(){
+        return "aboutproject";
+    }
+    @RequestMapping("/Pracownicy")
+    public String Pracownicy(){
+        return "Pracownicy";
+    }
 }

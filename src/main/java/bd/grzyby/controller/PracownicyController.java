d6db4c76dc12c9eca595d6860fafc111b4b6420d
Iwo Staykov <259259@student.pwr.edu.pl>
2025-01-13 00:12:25 +0100

    All basic views

---
+++ src/main/java/bd/grzyby/controller/PracownicyController.java
@@ -0,0 +1,25 @@
+package bd.grzyby.controller;
+
+import bd.grzyby.model.entity.Pracownik;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class PracownicyController {
+
+    private final PracownikService pracownikService;
+
+    public PracownicyController(PracownikService pracownikService) {
+        this.pracownikService = pracownikService;
+    }
+
+    @GetMapping("/Pracownicy")
+    public String showPracownicy(Model model) {
+        List<Pracownik> pracownicy = pracownikService.findAllPracownicy();
+        model.addAttribute("Pracownicy", pracownicy);
+        return "Pracownicy";
+    }
+}
